876e53a02e76f8f70196f9fffd25bf8d10dc42d2
apiszczukw <apiszczukw@gmail.com>
2025-02-27 08:27:37 +0100

    Add project files.

---
+++ UKW_sklep_czw/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UKW_sklep_czw.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
